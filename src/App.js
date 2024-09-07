4cd9f05c21ff5f24e1d7a93cd4ee0b14b5cafedb
junjuny0227 <junjuny0227@gmail.com>
2024-09-08 00:51:29 +0900

    Movie API

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+
 function App() {
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    axios
+      .get(
+        "https://yts.mx/api/v2/list_movies.json?minimum_rating=8.5&sort_by=year"
+      )
+      .then((response) => {
+        console.log(response);
+        setLoading(false);
+      });
+  }, []);
+
   return (
     <div>
       <h1>Movie App</h1>
+      {loading ? <h2>loading...</h2> : null}
     </div>
   );
 }
